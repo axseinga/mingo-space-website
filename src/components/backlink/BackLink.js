16548ae96fa7b9e5a3ba77758e8bd9e29e089f22
axseinga <urbanowicz.uk@gmail.com>
2023-03-29 18:11:57 +0200

    add scroll to top

--- src/components/backlink/BackLink.js
+++ src/components/backlink/BackLink.js
@@ -3,11 +3,14 @@ import { StyledBackLink } from "./BackLinks.styled";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const BackLink = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+  }
   return (
     <StyledBackLink>
       <BsFillArrowUpCircleFill
         className="BackLink-icon"
-        onClick={window.scrollTo(0, 0)}
+        onClick={handleScrollToTop}
       />
     </StyledBackLink>
   );
