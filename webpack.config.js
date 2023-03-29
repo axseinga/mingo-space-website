c8d235bf916183d87f777e985ff280dc9918e2ac
axseinga <urbanowicz.uk@gmail.com>
2023-03-29 15:10:02 +0200

    rewrote webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,106 @@
 const path = require("path");
-const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const DEV_MODE = process.env.NODE_ENV === "development";
 const SRC_DIR = path.resolve(__dirname, "src");
 const PUBLIC_DIR = path.resolve(__dirname, "public");
 const BUILD_DIR = path.resolve(__dirname, "build");
+const PUBLIC_PATH = process.env.PUBLIC_PATH ?? "/";
 
 module.exports = {
-    mode: DEV_MODE ? "development" : "production",
-    target: DEV_MODE ? "web" : "browserslist",
-    stats: "minimal",
-    devtool: DEV_MODE ? "eval-cheap-module-source-map" : "source-map",
-    devServer: {
-        port: 8081,
-        historyApiFallback: true,
-        hot: true,
-    },
-    entry: "./src/index.js",
-    output: {
-        filename: "[name].[contenthash:8].js",
-        chunkFilename: "[name].[contenthash:8].chunk.js",
-        path: BUILD_DIR,
-        clean: true,
-        publicPath: "/",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-react", "@babel/preset-env"],
-                        plugins: [
-                            "@babel/plugin-transform-runtime",
-                            "babel-plugin-macros",
-                            [
-                                "babel-plugin-styled-components",
-                                {
-                                    ssr: !DEV_MODE,
-                                    fileName: false,
-                                    displayName: DEV_MODE,
-                                    minify: !DEV_MODE,
-                                    pure: !DEV_MODE,
-                                },
-                            ],
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: DEV_MODE,
-                        },
-                    },
+  cache: {
+    type: "filesystem",
+  },
+  mode: DEV_MODE ? "development" : "production",
+  target: DEV_MODE ? "web" : "browserslist",
+  stats: "minimal",
+  devtool: DEV_MODE ? "eval-cheap-module-source-map" : "source-map",
+  entry: path.join(SRC_DIR, "index.js"),
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheCompression: false,
+              cacheDirectory: true,
+              presets: [
+                "@babel/preset-react",
+                "@babel/preset-env",
+              ],
+              plugins: [
+                "@babel/plugin-transform-runtime",
+                "babel-plugin-macros",
+                [
+                  "babel-plugin-styled-components",
+                  {
+                    ssr: !DEV_MODE,
+                    fileName: false,
+                    displayName: DEV_MODE,
+                    minify: !DEV_MODE,
+                    pure: !DEV_MODE,
+                  },
                 ],
+                ...(DEV_MODE ? ["react-refresh/babel"] : []),
+              ],
             },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                    },
-                ],
-            },
+          },
         ],
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.join(PUBLIC_DIR, "index.html"),
-            filename: "index.html",
-            inject: "body",
-            favicon: "./public/favicon.ico",
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: PUBLIC_DIR,
-                    to: BUILD_DIR,
-                    globOptions: {
-                        ignore: [path.join(PUBLIC_DIR, "index.html")],
-                    },
-                },
-            ],
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash:8].css",
-        }),
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: DEV_MODE,
+            },
+          },
+        ],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(PUBLIC_DIR, "index.html"),
+      filename: "index.html",
+      inject: "body",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: PUBLIC_DIR,
+          to: BUILD_DIR,
+          globOptions: {
+            ignore: [path.join(PUBLIC_DIR, "index.html")],
+          },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css",
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
+  output: {
+    filename: "[name].[contenthash:8].js",
+    chunkFilename: "[name].[contenthash:8].chunk.js",
+    path: BUILD_DIR,
+    clean: true,
+    publicPath: PUBLIC_PATH,
+  },
 };
